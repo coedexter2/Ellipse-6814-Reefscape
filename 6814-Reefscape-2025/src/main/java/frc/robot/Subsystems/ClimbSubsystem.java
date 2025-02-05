3a3fd1c55f72215705ba9ccd0306de49d3509a3c
tacoistaco1 <155571135+tacoistaco1@users.noreply.github.com>
2025-02-04 17:00:31 -0800

    Climb subsystem
    Created climb subsystem with set speed reset encoder and get encoder position.

--- 6814-Reefscape-2025/src/main/java/frc/robot/Subsystems/ClimbSubsystem.java
+++ 6814-Reefscape-2025/src/main/java/frc/robot/Subsystems/ClimbSubsystem.java
@@ -1,5 +1,44 @@
 package frc.robot.Subsystems;
 
-public class ClimbSubsystem {
+import java.beans.Encoder;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimbConstants;
+
+
+public class ClimbSubsystem extends SubsystemBase {
+    private final SparkMax m_climbMotor = new SparkMax(ClimbConstants.kClimbMotorID, MotorType.kBrushless);
+    private final RelativeEncoder m_climbEncoder = m_climbMotor.getEncoder();
     
+   
+    public ClimbSubsystem() {
+
+        m_climbMotor.configure(ClimbConstants.kClimbMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    }
+
+    public void setSpeed(double speed) {
+
+        m_climbMotor.set(speed);
+    }
+
+
+    public void resetEncoder() {
+        
+        m_climbEncoder.setPosition(0);
+    }
+
+
+    public double getEncoderPosition() {
+
+        return m_climbEncoder.getPosition();
+
+    }
+
 }
