0b6e17be5b7303e9fc3759b0d8b876fd80b0fa2d
coedexter2 <117695596+coedexter2@users.noreply.github.com>
2025-02-07 16:29:34 -0800

    Pose Estimator Rewrite

--- 6814-Reefscape-2025/src/main/java/frc/robot/RobotContainer.java
+++ 6814-Reefscape-2025/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Commands.LimelightUpdate;
 import frc.robot.Commands.SwerveJoystickCmd;
 import frc.robot.Subsystems.SwerveSubsystem;
 
@@ -15,14 +17,15 @@ public class RobotContainer {
 
   private final Joystick m_Joysitck = new Joystick(Constants.OIConstants.kDriverControllerPort);
 
+  private final ParallelCommandGroup Swerve = new ParallelCommandGroup(new SwerveJoystickCmd(
+    m_Swerve,
+    () -> -m_Joysitck.getRawAxis(Constants.OIConstants.kDriverXAxis),
+    () -> m_Joysitck.getRawAxis(Constants.OIConstants.kDriverYAxis),
+    () -> -m_Joysitck.getRawAxis(Constants.OIConstants.kDriverRotAxis),
+    () -> !m_Joysitck.getRawButton(Constants.OIConstants.kDriverFieldOrientedButtonIdx)), new LimelightUpdate());
 
   public RobotContainer() {
-    m_Swerve.setDefaultCommand(new SwerveJoystickCmd(
-      m_Swerve,
-      () -> -m_Joysitck.getRawAxis(Constants.OIConstants.kDriverXAxis),
-      () -> m_Joysitck.getRawAxis(Constants.OIConstants.kDriverYAxis),
-      () -> -m_Joysitck.getRawAxis(Constants.OIConstants.kDriverRotAxis),
-      () -> !m_Joysitck.getRawButton(Constants.OIConstants.kDriverFieldOrientedButtonIdx)));
+    m_Swerve.setDefaultCommand(Swerve);
 
 
     configureBindings();
